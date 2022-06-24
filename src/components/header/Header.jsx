ad21796e811808104629abe467ad30b820600ec7
kashkaryan <kachik@seas.upenn.edu>
2022-06-24 11:09:23 -0700

    finished new header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,8 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
+import {AiOutlineArrowRight} from 'react-icons/ai'
+import {FcGoogle} from 'react-icons/fc'
 
 const Header = () => {
   return (
@@ -10,14 +12,15 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Kachik Ashkaryan</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className='text-light'>Fullstack Developer @ <FcGoogle size={30} className='icon__google'/>oogle</h5>
         <CTA/>
         <HeaderSocials/>
         <div className="me">
           <img src={ME} alt="me" />
         </div>
-
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        <div className="scroll__down">
+          <a href="#about">Scroll Down <AiOutlineArrowRight href="#about" className='down__arrow'/></a>
+        </div>
       </div>
     </header>
   )
